18a72eb4c495b289413592481e2838b8b8d55335
mohammad mohseni <mohammad.mohseni468@gmail.com>
2021-11-07 12:14:17 +0330

    props in spa

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Porducts from "./components/Porducts";
 import Blogs from "./components/Blogs";
 import AboutUs from "./components/AboutUs";
 import MyNavbar from "./components/MyNavbar";
-import { Route, Link } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 // import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 
 import "./App.scss";
@@ -15,10 +15,25 @@ function App() {
     <div>
       <MyNavbar />
       <div>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/Porducts" component={Porducts} />
-        <Route path="/Blogs" component={Blogs} />
-        <Route path="/AboutUs" component={AboutUs} />
+        <Switch>
+          <Route
+            path="/Porducts"
+            render={(props) => <Porducts name="mohammad" {...props} />}
+          />
+          <Route
+            path="/Blogs"
+            render={(props) => <Blogs name="mohammad" {...props} />}
+          />
+          <Route
+            path="/AboutUs"
+            render={(props) => <AboutUs name="mohammad" {...props} />}
+          />
+          <Route
+            path="/"
+            exact
+            render={(props) => <HomePage name="mohammad" {...props} />}
+          />
+        </Switch>
       </div>
     </div>
   );

d2f3203f9f51db4a45fc916475fe1aa271c2239c
mohammad mohseni <mohammad.mohseni468@gmail.com>
2021-11-08 15:08:57 +0330

    parse query

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
+import queryString from "query-string";
 
 export default class Blogs extends Component {
+  parseQuery = () => {
+    const result = queryString.parse(this.props.location.search);
+    console.log(result);
+  };
   render() {
     return (
       <div>
         <Container>
           <h1>Blogs</h1>
+          <Button variant="info" onClick={this.parseQuery}>
+            Info
+          </Button>
         </Container>
       </div>
     );
